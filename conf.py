5ec0c39744e984ebcc83c5d5c49cc2e42fd426eb
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2024-10-11 14:11:10 +0100

    docs: expand intersphinx inventory

--- conf.py
+++ conf.py
@@ -36,20 +36,19 @@ html_static_path = ["quantinuum-sphinx/_static", "_static"]
 html_favicon = "quantinuum-sphinx/_static/assets/quantinuum_favicon.svg"
 
 
-pytketdoc_base = "https://tket.quantinuum.com/api-docs/"
+pytketdoc_base = "https://docs.quantinuum.com/tket/"
+ext_url = pytketdoc_base + "extensions/"
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
-    "pytket": (pytketdoc_base, None),
-    "qiskit": ("https://qiskit.org/documentation/", None),
-    "qulacs": ("http://docs.qulacs.org/en/latest/", None),
-    "pytket-qiskit": ("https://tket.quantinuum.com/extensions/pytket-qiskit/", None),
-    "pytket-quantinuum": (
-        "https://tket.quantinuum.com/extensions/pytket-quantinuum/",
-        None,
-    ),
-    "pytket-qujax": ("https://tket.quantinuum.com/extensions/pytket-qujax/", None),
-    "pytket-cirq": ("https://tket.quantinuum.com/extensions/pytket-cirq/", None),
+    "pytket": (pytketdoc_base + "api-docs/", None),
+    "qiskit": ("https://docs.quantum.ibm.com/api/qiskit", None),
+    "pytket-qiskit": (ext_url + "pytket-qiskit/", None),
+    "pytket-quantinuum": (ext_url + "pytket-quantinuum/", None,),
+    "pytket-pennylane": (ext_url + "pytket-pennylane/", None),
+    "pytket-qujax": (ext_url + "pytket-qujax/", None),
+    "pytket-cirq": (ext_url + "pytket-cirq/", None),
+    "pytket-braket": (ext_url + "pytket-braket/", None),
     "sympy": ("https://docs.sympy.org/latest/", None),
 }
 
